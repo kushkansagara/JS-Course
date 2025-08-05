36677cedbe402678fe9592b53bec46014467614c
kushkansagara <kush02032004@gmail.com>
2025-08-05 19:56:45 +0530

    Add first 2 folder

---
+++ 01 JavaScript Fundamentals – Part 1/script.js
@@ -0,0 +1,28 @@
+// let js = "kush";
+// console.log(1 + 14 + 15);
+
+// console.log("jones");
+// console.log(23);
+
+// let firstName = "kush";
+// let PI = 3.1415;
+
+// console.log(typeof 123);
+// console.log(typeof "kush");
+// console.log(typeof true);
+
+// let age = 20;
+// age = 21;
+
+// const year = 2004;
+// // year = 2005;
+
+// var a = 124;
+// a = 45;
+
+let a = "kush";
+let b = "kansagara";
+$string = `I am a ${a} ${b}`;
+console.log($string);
+console.log(`kush
+kansagara`);
---
+++ 02 JavaScript Fundamentals – Part 2/script.js
@@ -0,0 +1,48 @@
+// "use strict";
+
+// let hadLicense = false;
+// let passTest = true;
+
+// if (passTest) {
+//   hadLicense = true;
+// }
+// if (hadLicense) {
+//   console.log("drive");
+// }
+
+// function logger() {
+//   console.log("My name is Kush");
+// }
+
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+// const st = fruitProcessor(5, 0);
+// console.log(st);
+
+/*
+function calcAge(birthyear) {
+  return 2025 - birthyear;
+}
+
+const age = calcAge(2004);
+console.log(age);
+
+const calcAge2 = function (birthyear) {
+  return 2025 - birthyear;
+};
+
+const age2 = calcAge2(2004);
+
+const calcAge3 = (birthyear) => 2025 - birthyear;
+
+const YearsUntilRetirement = (birthyear, firstName) => {
+    const age = 2025 - birthyear;
+    const retirement = 35 - age;
+    return `${firstName} retires in ${retirement} years.`;
+    //   return retirement;
+};
+*/
